4f437f9d320b3ee70be67185713df821e915f077
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2022-08-31 02:22:33 +0300

    add Emojis.java

---
+++ src/main/java/timetablebot/enums/Emojis.java
@@ -0,0 +1,18 @@
+package timetablebot.enums;
+
+import com.vdurmont.emoji.EmojiParser;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum Emojis {
+    GRINNING_FACE(EmojiParser.parseToUnicode(":grinning:")),
+    ALARM_CLOCK(EmojiParser.parseToUnicode(":alarm_clock:")),
+    SMILING_FACE(EmojiParser.parseToUnicode(":relaxed:")),
+    NO_MONTH(EmojiParser.parseToUnicode(":no_mouth:")),
+    SCROLL(EmojiParser.parseToUnicode(":scroll:")),
+    PAGE_FACING_UP(EmojiParser.parseToUnicode(":page_facing_up:")),
+    WAVE_HAND(EmojiParser.parseToUnicode(":wave:"));
+    private final String emojiCode;
+}
