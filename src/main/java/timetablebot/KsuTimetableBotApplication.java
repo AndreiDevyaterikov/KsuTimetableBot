0a8f74c5a1ec216eb62dac50234ca0e1bbcbb4ef
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2022-08-31 02:05:16 +0300

    init project

---
+++ src/main/java/timetablebot/KsuTimetableBotApplication.java
@@ -0,0 +1,13 @@
+package timetablebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsuTimetableBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsuTimetableBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/timetablebot/KsuTimetableBotApplicationTests.java
@@ -0,0 +1,13 @@
+package timetablebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsuTimetableBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
