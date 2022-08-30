925b2bfd0369d3e1a3af01876b71120427718d1f
AndreiDevyaterikov <devyaterikov.andrei@gmail.com>
2022-08-31 02:19:50 +0300

    add botToken and botName

---
+++ src/main/java/timetablebot/TelegramBot.java
@@ -0,0 +1,31 @@
+package timetablebot;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    @Value("${telegram.token}")
+    String botToken;
+
+    @Value("${telegram.name}")
+    String botUsername;
+
+    @Override
+    public String getBotUsername() {
+        return botUsername;
+    }
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
